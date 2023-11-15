e3e5bdf187e9089c01666ece793b8d0f2e9c252f
cryptohero92 <alwaysflexibleman@gmail.com>
2023-11-15 13:45:09 +0900

    working on left panel

--- packages/frontend/src/Component/Market/LeftPanel.js
+++ packages/frontend/src/Component/Market/LeftPanel.js
@@ -2,22 +2,12 @@ import * as React from 'react';
 import ListSubheader from '@mui/material/ListSubheader';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import Collapse from '@mui/material/Collapse';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
-import SendIcon from '@mui/icons-material/Send';
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import StarBorder from '@mui/icons-material/StarBorder';
+import { RadioGroup, Radio, FormControlLabel } from '@mui/material';
+
+import CollapsableItem from './CollapsableItem';
 
 export const LeftPanel = () => {
-  const [open, setOpen] = React.useState(true);
-
-  const handleClick = () => {
-    setOpen(!open);
-  };
 
   return (
     <List
@@ -30,35 +20,66 @@ export const LeftPanel = () => {
         </ListSubheader>
       }
     >
-      <ListItemButton>
-        <ListItemIcon>
-          <SendIcon />
-        </ListItemIcon>
-        <ListItemText primary="Sent mail" />
-      </ListItemButton>
-      <ListItemButton>
-        <ListItemIcon>
-          <DraftsIcon />
-        </ListItemIcon>
-        <ListItemText primary="Drafts" />
-      </ListItemButton>
-      <ListItemButton onClick={handleClick}>
-        <ListItemIcon>
-          <InboxIcon />
-        </ListItemIcon>
-        <ListItemText primary="Inbox" />
-        {open ? <ExpandLess /> : <ExpandMore />}
-      </ListItemButton>
-      <Collapse in={open} timeout="auto" unmountOnExit>
+      <CollapsableItem title="Volume">
+        <RadioGroup
+          aria-labelledby="demo-radio-buttons-group-label"
+          defaultValue="any"
+          name="radio-buttons-group"
+        >
+          <FormControlLabel value="any" control={<Radio />} label="Any" />
+          <FormControlLabel value="under_10000" control={<Radio />} label="Under $10,000" />
+          <FormControlLabel value="10000_50000" control={<Radio />} label="$10,000 - $50,000" />
+          <FormControlLabel value="50000_100000" control={<Radio />} label="$50,000 - $100,000" />
+          <FormControlLabel value="over_100000" control={<Radio />} label="Over $100,000" />
+          <FormControlLabel value="custom" control={<Radio />} label="Custom" />
+        </RadioGroup>
+      </CollapsableItem>
+      <CollapsableItem title="Liquidity">
+        <RadioGroup
+          aria-labelledby="demo-radio-buttons-group-label"
+          defaultValue="any"
+          name="liquidity-radio-buttons-group"
+        >
+          <FormControlLabel value="any" control={<Radio />} label="Any" />
+          <FormControlLabel value="under_5000" control={<Radio />} label="Under $5,000" />
+          <FormControlLabel value="5000_10000" control={<Radio />} label="$5,000 - $10,000" />
+          <FormControlLabel value="10000_50000" control={<Radio />} label="$10,000 - $50,000" />
+          <FormControlLabel value="over_50000" control={<Radio />} label="Over $50,000" />
+          <FormControlLabel value="custom" control={<Radio />} label="Custom" />
+        </RadioGroup>
+      </CollapsableItem>
+      <CollapsableItem title="End Date">
+        <RadioGroup
+          aria-labelledby="demo-radio-buttons-group-label"
+          defaultValue="any"
+          name="liquidity-radio-buttons-group"
+        >
+          <FormControlLabel value="any" control={<Radio />} label="Any" />
+          <FormControlLabel value="end_today" control={<Radio />} label="Ends today" />
+          <FormControlLabel value="end_this_week" control={<Radio />} label="Ends this week" />
+          <FormControlLabel value="end_this_month" control={<Radio />} label="Ends this month" />
+          <FormControlLabel value="custom" control={<Radio />} label="Custom" />
+        </RadioGroup>
+      </CollapsableItem>
+      <CollapsableItem title="Status">
+        <RadioGroup
+          aria-labelledby="demo-radio-buttons-group-label"
+          defaultValue="active"
+          name="liquidity-radio-buttons-group"
+        >
+          <FormControlLabel value="all" control={<Radio />} label="All" />
+          <FormControlLabel value="active" control={<Radio />} label="Active" />
+          <FormControlLabel value="resolved" control={<Radio />} label="Resolved" />
+        </RadioGroup>
+      </CollapsableItem>
+      <CollapsableItem title="Categories">
         <List component="div" disablePadding>
           <ListItemButton sx={{ pl: 4 }}>
-            <ListItemIcon>
-              <StarBorder />
-            </ListItemIcon>
             <ListItemText primary="Starred" />
           </ListItemButton>
         </List>
-      </Collapse>
+      </CollapsableItem>
+      
     </List>
   );
 }
