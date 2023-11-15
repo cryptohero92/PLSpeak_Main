7b10936dad7ca8cbc80da64972979118af0d8209
cryptohero92 <alwaysflexibleman@gmail.com>
2023-11-15 11:30:42 +0900

    adopted market component, ready to replace its content

--- packages/frontend/src/Page/Markets.js
+++ packages/frontend/src/Page/Markets.js
@@ -1,15 +1,16 @@
 import { Box } from '@mui/system'
 import { useTheme } from '@mui/material/styles';
 import Header from '../Component/Header'
+import { Market } from '../Component/Market'
 
-const Market = () => {
+const Markets = () => {
 	const theme = useTheme();
 	return (
 		<Box sx={{ bgcolor: theme.palette.mode === 'dark' ? '#1c2033' : '#E5E5E5' }}>
 			<Header />
-			<div>Markets page</div>
+			<Market />
 		</Box>
 	)
 }
 
-export default Market;
+export default Markets;
